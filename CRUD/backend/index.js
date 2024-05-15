b944bee1b208beb80bf29483f7402ababd1cb4d7
KGCAS STUDENTS <casstudent@kgisledu.com>
2024-05-15 12:26:27 +0530

    node

---
+++ CRUD/backend/index.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const users=require('./routes/user')
+const mongoose=require('mongoose')
+const cors=require('cors')
+const app=express()
+app.use(cors())
+mongoose.connect('mongodb://127.0.0.1:27017/Userdatas')
+
+mongoose.connection.on('connected',()=>{
+    console.log("Mongodb successfully connected")
+})
+app.get('/',(req,res)=>{
+    res.send("Backend server")
+})
+app.use('/users',users)
+app.listen(3001,()=>{
+    console.log("Server is running on port 3001")
+})
